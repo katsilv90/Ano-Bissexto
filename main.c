d7f37b56fe5b887bafddc9bff0f4f5902f23301b
Katherine Silvestre <katherinesilv@gmail.com>
2025-11-06 12:12:56 +0000

    Update main.c

--- main.c
+++ main.c
@@ -16,27 +16,8 @@ int main()
         printf("Ano %d nao e bissexto",ano);
     }
 
-
-
-    /** \brief
-     *  Outra forma de fazer
-     * \param
-     *
-     if(ano%400==0){
-        printf("Ano %d e bissexto",ano);
-    }
-    else if (ano%100==0){
-        printf("Ano %d nao e bissexto",ano);
-    }
-    else if (ano%4==0){
-        printf("Ano %d  e bissexto",ano);
-    }
-    else  {
-        printf("Ano %d nao e bissexto",ano);
-    }
-     */
-
     return 0;
 }
+
 
 
